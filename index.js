775ec8be4f2759cb46b814d5d79e9f36eef1618d
Christian Ahn <kwanghoonahn@gmail.com>
2017-10-25 22:15:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ var kittens = ["Milo", "Otis", "Garfield"];
 // appends a kitten to the end of the kitten array (mutate)
 function destructivelyAppendKitten(name) {
   kittens.push(name);
-  return kittens
+  return kittens;
+}
+
+// prepends a kitten to the beginning of the kittens array
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
 }
